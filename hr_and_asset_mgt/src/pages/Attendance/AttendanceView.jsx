34eca1fcf5034f6ab2421a3f0a37a59b9129423d
anoop2000 <88547121+anoop2000@users.noreply.github.com>
2025-12-18 23:52:50 +0530

    attendance section started

--- hr_and_asset_mgt/src/pages/Attendance/AttendanceView.jsx
+++ hr_and_asset_mgt/src/pages/Attendance/AttendanceView.jsx
@@ -1,8 +1,16 @@
 
 import React from "react";
+import AttendanceHeader from "./AttendanceHeader";
+import AttendanceStats from "./AttendanceStats";
 
 function Attendance() {
-    return <div>Attendance Page</div>;
+    return (
+    <div>
+        <AttendanceHeader />
+        <AttendanceStats />
+    </div>
+    )
 }
 
 export default Attendance;
+
